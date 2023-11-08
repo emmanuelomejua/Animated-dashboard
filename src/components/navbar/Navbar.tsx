78cf4f3ddaf5b5a7878934b8f1e46ca86422f6d2
Emmanuel Omejua <omejuaifeanyi2@gmail.com>
2023-11-08 02:11:41 +0100

    navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,14 +1,29 @@
-import React from 'react'
+import './navbar.scss'
 
 const Navbar = () => {
   return (
     <nav className='navbar'>
       <section className='logo'>
-        <img src='logo.jpg' alt='' height={50}/>
+        <img src='logo.jpg' alt='' height={24}/>
         <span>Admin</span>
       </section>
-      <section className='links'>
 
+      <section className='icons'>
+        <img src="/search.svg" alt="" className="icon" />
+        <img src="/app.svg" alt="" className="icon" />
+        <img src="/expand.svg" alt="" className="icon" />
+
+        <div className="notification">
+          <img src="/notifications.svg" alt="" className="icon" />
+          <span>3</span>
+        </div>
+
+        <div className="user">
+          <img src="/admin.jpg" alt="" className="img" />
+          <span>Chi</span>
+
+        </div>
+        <img src="/setting.svg" alt="" className="icon" />
       </section>
     </nav>
   )
